6d3159819d52236f0ffc6b94a4e1b4bb197e7046
zloeezh <zloeezh@dzmitrys-air.cust.communityfibre.co.uk>
2024-03-03 03:39:55 +0000

    Added considareation to take into account GIf frame delay time to support "slowly" animated files

--- src/co/uk/zloezh/led/thread/DisplayGif.java
+++ src/co/uk/zloezh/led/thread/DisplayGif.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
 import javax.imageio.stream.ImageInputStream;
 
 import org.apache.logging.log4j.LogManager;
@@ -45,6 +46,8 @@ public class DisplayGif extends RenderImage{
 
 	public void run() {
 		logger.info("Started");
+		long lastFrameTime = 0, waitTime = 0;
+		//long waitTime = 0;
 	        while (true) {
 	        	logger.info("." + this.isPauseThreadFlag());
 	            	this.checkForPaused();
@@ -72,19 +75,29 @@ public class DisplayGif extends RenderImage{
 							        IIOMetadata metadata = reader.getImageMetadata(i);
 							        Node tree = metadata.getAsTree("javax_imageio_gif_image_1.0");
 							        NodeList children = tree.getChildNodes();
+							        
+				                    Map<String, Integer> imageAttr = new HashMap<String, Integer>();
+				                    IIOMetadataNode graphicsControlExtensionNode = getNode((IIOMetadataNode)tree, "GraphicControlExtension");
+				                    imageAttr.put("delayTime", Integer.valueOf(graphicsControlExtensionNode.getAttribute("delayTime"))*10);
+				                    //System.out.println("delayTime 001: " + );
 			
 							        for (int j = 0; j < children.getLength(); j++) {
 
 							            Node nodeItem = children.item(j);
+							          //  System.out.println("nodeItem.getNodeName() 0012: " + nodeItem.getNodeName());
 							            if(nodeItem.getNodeName().equals("ImageDescriptor")){
-							                Map<String, Integer> imageAttr = new HashMap<String, Integer>();
+							               
+							                
+							                
 			
 							                for (int k = 0; k < imageatt.length; k++) {
 
 							                    NamedNodeMap attr = nodeItem.getAttributes();
 							                    Node attnode = attr.getNamedItem(imageatt[k]);
 							                    imageAttr.put(imageatt[k], Integer.valueOf(attnode.getNodeValue()));
+    
 							                }
+							                logger.debug("Frame Attributes: " + imageAttr);
 							                if(i==0){
 							                    master = new BufferedImage(imageAttr.get("imageWidth"), imageAttr.get("imageHeight"), BufferedImage.TYPE_INT_ARGB);
 
@@ -95,15 +108,21 @@ public class DisplayGif extends RenderImage{
 							        		long[] hexArray = instance.hexFromImage(master);
 							        		LEDFrame frame = instance.generateLedFrame(hexArray);
 										   
-
+							        		waitTime = imageAttr.get("delayTime") - (System.currentTimeMillis() - lastFrameTime);
+							        		logger.debug("waitTime: " + waitTime);
+							        		if(waitTime > 0) {
+							        			logger.debug("Waiting: " + waitTime);
+							        			this.sleep(waitTime);
+							        			
+							        		}
 							        		HTTPUtils.SendFrameViaUDP(frame, screen);
-	
+							        		lastFrameTime = System.currentTimeMillis();
 							        		break;
 							            }
 							        }
 						    	}// ImageIO.write(master, "GIF", new File( i + ".gif")); 
 						    
-						} catch (IOException e) {
+						} catch (IOException | InterruptedException e) {
 						    e.printStackTrace();
 						}
 	            	//}
@@ -111,5 +130,19 @@ public class DisplayGif extends RenderImage{
 	            
 	        }
 	    }
+	
+		 private static IIOMetadataNode getNode(IIOMetadataNode rootNode, String nodeName) {
+		        int nNodes = rootNode.getLength();
+		        for (int i = 0; i < nNodes; i++) {
+		        	//System.out.println("NodeLoop 001: " + rootNode.item(i).getNodeName());
+		            if (rootNode.item(i).getNodeName().compareToIgnoreCase(nodeName)== 0) {
+		            	//System.out.println("getNodeName " + rootNode.item(i).getNodeName());
+		            	return((IIOMetadataNode) rootNode.item(i));
+		            }
+		       }
+		        IIOMetadataNode node = new IIOMetadataNode(nodeName);
+		        rootNode.appendChild(node);
+		        return(node);
+		  }
 
 }
