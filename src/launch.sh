8c5f98a6036ad36159576d02f7f808e0114cff0b
ShparkyEg <ShparkyEg@DESKTOP-KPQMP9Q.cust.communityfibre.co.uk>
2024-04-08 00:55:57 +0100

    Minor build related changes

--- src/launch.sh
+++ src/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "LedScreen-0.5-jar-with-dependencies.jar" co.uk.zloezh.led.ArduinoLEDTool
+java -Dlog4j.configurationFile='/appfiles/log4j2.xml' -cp "LedScreen-0.5-jar-with-dependencies.jar" co.uk.zloezh.led.ArduinoLEDTool
